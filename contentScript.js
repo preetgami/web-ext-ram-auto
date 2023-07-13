5cc303db2900784e4ff308de16efd0022cd6c429
preetgami <preetgami20@gmail.com>
2023-07-13 16:34:59 -0600

    downloads

--- contentScript.js
+++ contentScript.js
@@ -1,35 +1,40 @@
-
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
 
-    if (msg.command =="runCommands"){
+    if (msg.command == "runCommands") {
         console.log('start commands');
         window.Scrapper = []
 
-        var scapeObject=msg.data;
+        var scapeObject = msg.data;
         console.log(scapeObject)
         console.log("scapeObject")
 
-        getNext(scapeObject,0)
+        getNext(scapeObject, 0)
 
 
     }
 })
-function getNext(obj,index){
+
+
+
+function getNext(obj, index) {
     console.log("type");
     console.log(!(typeof obj[index] !== "undefined" && index < obj.length))
 
     console.log("here we arewindow.Scrapper");
     console.log(index);
 
-    if (typeof obj[index] !== "undefined" && index<obj.length) {
+    if (typeof obj[index] !== "undefined" && index < obj.length) {
         if (obj[index].type === "wait") {
             waitEvent(obj, index);
         } else if (obj[index].type === "save") {
             saveEvent(obj, index);
         } else if (obj[index].type === "enter") {
             enterEvent(obj, index);
+        }else if(obj[index].type=="append"){
+            appendEvent(obj,index)
         }
-    } 
+
+    }
     else {
         console.log("run complete");
         chrome.runtime.sendMessage({ command: "run-complete", data: window.Scrapper });
@@ -42,12 +47,12 @@ function waitEvent(obj, index) {
     console.log("in wait")
     console.log(obj[index])
 
-    var item=obj[index];
+    var item = obj[index];
 
-    var wait=parseInt(item.one)
-    console.log(wait,"wait time")
+    var wait = parseInt(item.one)
+    console.log(wait, "wait time")
 
-    setTimeout(function(){
+    setTimeout(function () {
         getNext(obj, (index + 1))
 
     }, wait)
@@ -71,12 +76,19 @@ function saveEvent(obj, index) {
 function enterEvent(obj, index) {
     console.log("in enter")
     var item = obj[index];
+    
+
     console.log(item.one)
     var element = document.getElementById(item.one);
     if (element) {
         var value = element.innerText;
         window.Scrapper.push(value);
+        chrome.runtime.sendMessage({ command: "download-file", data: window.Scrapper });
+        console.log("##################slkfnsfdsm#################")
+
+        
     }
+    
     var image = document.querySelector('.fancybox');
     /*
     if (image) {
@@ -89,34 +101,42 @@ function enterEvent(obj, index) {
         link.click();
     }
     */
-    function exportExcel(){
-        const workbook= new ExcelJS.Workbook()
-        const worksheet= workbook.addWorksheet("part data");
-        const headerRow=worksheet.addRow([])
+    
+    
 
-        const headerCells=element.getElementById("th")
-        for (let i = 0; i < headerCells.length;i++){
-            headerRow.getCell(i+1).value=headerCells[i].innerText
-        }
-        const rows=element.getElementById("td")
+    getNext(obj, (index + 1))
+}
 
-        for(let i=0;i<rows.length;i++){
-            const cells=rows[i].getElementsbyTagNme("td")
-            const rowData=[]
-            for(let j=0;j<cells.length;j++){
-                rowData.push(cells[j].innerText)
-            }
-            worksheet.addRow(rowData)
-        }
+function appendEvent(obj, index) {
+    console.log("in enter")
+    var item = obj[index];
 
-        workbook.xlsx.writeBuffer().then((buffer)=>{
-            const blob=new Blob([buffer],{
-                type:"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-            })
-            saveAs(blob,"out.xlsx")
-        })
+
+    console.log(item.one)
+    var element = document.getElementById(item.one);
+    if (element) {
+        var value = element.innerText;
+        window.Scrapper.push(value);
+        chrome.runtime.sendMessage({ command: "append-data", data: window.Scrapper });
+        console.log("sent append message")
+
+
     }
-    exportExcel()
+
+    var image = document.querySelector('.fancybox');
+    /*
+    if (image) {
+        var href = image.getAttribute('href');
+        var filename = href.split('/').pop();
+
+        var link = document.createElement('a');
+        link.href = href;
+        link.download = filename;
+        link.click();
+    }
+    */
+
+
 
     getNext(obj, (index + 1))
 }
--- popup.js
+++ popup.js
@@ -6,6 +6,49 @@ chrome.runtime.onMessage.addListener((msg,sender,response)=>{
 
     }
 })
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+    if (msg.command === "download-file") {
+        console.log("###################################")
+        var downloadData = msg.data.join("\n"); // Join values with newline separator
+
+        var element = document.createElement("a");
+        element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(downloadData));
+        element.setAttribute("download", "scrapper_data.txt");
+
+        element.style.display = "none";
+        document.body.appendChild(element);
+
+        element.click();
+
+        document.body.removeChild(element);
+    }
+});
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+    if (msg.command === "append-data") {
+        var downloadData = msg.data.join("\n"); // Join values with newline separator
+
+        chrome.storage.local.get("scrapperData", (result) => {
+            var existingData = result.scrapperData || "";
+            var newData = existingData + downloadData + "\n";
+            console.log("get in son")
+            chrome.storage.local.set({ scrapperData: newData }, () => {
+                console.log("Data appended successfully");
+
+                // Create data URL for the file content
+                var dataURL = "data:text/plain;charset=utf-8," + encodeURIComponent(newData);
+
+                // Trigger the download
+                chrome.downloads.download({
+                    url: dataURL,
+                    filename: "scrapper_data.txt",
+                    saveAs: false
+                });
+            });
+        });
+    }
+});
+
+
 
 function commandObject(){
     var commandsArray=[
@@ -71,4 +114,38 @@ function saveObject() {
 
 document.querySelector(".save_data").addEventListener("click", function () {
     saveObject();
+})
+
+function appendObject() {
+    var commandsArray = [
+        {
+            type: "wait",
+            one: "500",
+            two: ""
+        }, {
+            type: "append",
+            one: "gf-result-table",
+            two: ""
+        },
+
+        {
+            type: "wait",
+            one: "500",
+            two: ""
+        }
+
+
+    ]
+    console.log(commandsArray)
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var activeTab = tabs[0];
+        var obj = commandsArray;
+        //console.log(activeTab, obj);
+        chrome.tabs.sendMessage(activeTab.id, { command: "runCommands", data: obj });
+
+    });
+}
+
+document.querySelector(".append_data").addEventListener("click", function () {
+    appendObject();
 })
