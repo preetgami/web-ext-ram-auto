fc133f4fa4b729ff03b98c8a04edb0d6f21c8944
preetgami <preetgami20@gmail.com>
2023-07-12 21:15:21 -0600

    new

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,4 @@
+
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
 
     if (msg.command =="runCommands"){
@@ -21,9 +22,7 @@ function getNext(obj,index){
     console.log(index);
 
     if (typeof obj[index] !== "undefined" && index<obj.length) {
-        if (obj[index].type === "click") {
-            clickEvent(obj, index);
-        } else if (obj[index].type === "wait") {
+        if (obj[index].type === "wait") {
             waitEvent(obj, index);
         } else if (obj[index].type === "save") {
             saveEvent(obj, index);
@@ -54,49 +53,70 @@ function waitEvent(obj, index) {
     }, wait)
 }
 
-function clickEvent(obj,index){
-    var item = obj[index];
-    document.querySelector(item.one).click()
-    getNext(obj, (index+1))
-}
-
 function saveEvent(obj, index) {
-    console.log("in save")
-
+    console.log("in check")
     var item = obj[index];
     console.log(item.one)
-
     var element = document.getElementById(item.one);
     if (element) {
         var value = element.innerText;
         window.Scrapper.push(value);
     }
-
-    var image = document.querySelector('.fancybox');
-    if (image) {
-        var href = image.getAttribute('href');
-        var filename = href.split('/').pop(); // Extract the filename from the href
-
-        // Create a dynamic anchor element
-        var link = document.createElement('a');
-        link.href = href;
-        link.download = filename;
-
-        // Programmatically trigger a click event on the anchor element
-        link.click();
-    }
-
     console.log("value")
-
     console.log(element)
     console.log("value")
-
-
     getNext(obj, (index + 1))
 }
 
 function enterEvent(obj, index) {
+    console.log("in enter")
     var item = obj[index];
-    var value = document.querySelector(item.one).value=item.two;
-    getNext(obj,( index + 1))
+    console.log(item.one)
+    var element = document.getElementById(item.one);
+    if (element) {
+        var value = element.innerText;
+        window.Scrapper.push(value);
+    }
+    var image = document.querySelector('.fancybox');
+    /*
+    if (image) {
+        var href = image.getAttribute('href');
+        var filename = href.split('/').pop();
+
+        var link = document.createElement('a');
+        link.href = href;
+        link.download = filename;
+        link.click();
+    }
+    */
+    function exportExcel(){
+        const workbook= new ExcelJS.Workbook()
+        const worksheet= workbook.addWorksheet("part data");
+        const headerRow=worksheet.addRow([])
+
+        const headerCells=element.getElementById("th")
+        for (let i = 0; i < headerCells.length;i++){
+            headerRow.getCell(i+1).value=headerCells[i].innerText
+        }
+        const rows=element.getElementById("td")
+
+        for(let i=0;i<rows.length;i++){
+            const cells=rows[i].getElementsbyTagNme("td")
+            const rowData=[]
+            for(let j=0;j<cells.length;j++){
+                rowData.push(cells[j].innerText)
+            }
+            worksheet.addRow(rowData)
+        }
+
+        workbook.xlsx.writeBuffer().then((buffer)=>{
+            const blob=new Blob([buffer],{
+                type:"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            })
+            saveAs(blob,"out.xlsx")
+        })
+    }
+    exportExcel()
+
+    getNext(obj, (index + 1))
 }
--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener((msg,sender,response)=>{
     if (msg.command=="run-complete"){
         document.querySelector("textarea").value=JSON.stringify(msg.data)
         document.querySelector("textarea").style.display="block"
-        alert("complete,lkd");
+        alert("completed search. Check if we found something");
 
     }
 })
@@ -38,3 +38,37 @@ document.querySelector(".check_data").addEventListener("click",function(){
     commandObject();
 })
 
+
+function saveObject() {
+    var commandsArray = [
+        {
+            type: "wait",
+            one: "500",
+            two: ""
+        }, {
+            type: "enter",
+            one: "gf-result-table",
+            two: ""
+        },
+
+        {
+            type: "wait",
+            one: "500",
+            two: ""
+        }
+
+
+    ]
+    console.log(commandsArray)
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var activeTab = tabs[0];
+        var obj = commandsArray;
+        //console.log(activeTab, obj);
+        chrome.tabs.sendMessage(activeTab.id, { command: "runCommands", data: obj });
+
+    });
+}
+
+document.querySelector(".save_data").addEventListener("click", function () {
+    saveObject();
+})
